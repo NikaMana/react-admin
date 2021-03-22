75ad945eaee45e1aa08564ccb66e5e333d51873f
Nika <delmare@mail.ru>
2021-03-22 16:39:36 +0300

    react-admin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Admin, Resource } from 'react-admin';
+import { UserList, UserEdit, UserCreate } from './users';
+import jsonServerProvider from 'ra-data-json-server';
+import authProvider from './authProvider';
+import Dashboard from './Dashboard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+const App = () => (
+<Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
+  <Resource name='users' list={UserList} edit={UserEdit} create={UserCreate} />
+</Admin>
+);
 
 export default App;
